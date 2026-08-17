ca735e4bea27c00e47b48e3fbab9d4ef6e2ab7cf
binhv005 <vtb22522005@gmail.com>
2026-08-17 10:08:56 +0700

    chore: update dependencies and server configuration

--- backend/server.js
+++ backend/server.js
@@ -1,54 +1,50 @@
 require("dotenv").config();
 
+const http = require("http");
 const express = require("express");
 const connectDB = require("./config/database");
 const cors = require("cors");
 const path = require("path");
+const cookieParser = require("cookie-parser");
 
 const productRoutes = require("./routes/productRoutes");
 const categoryRoutes = require("./routes/categoryRoutes");
+const authRoutes = require("./routes/authRoutes");
+const conversationRoutes = require("./routes/conversationRoutes");
+const messageRoutes = require("./routes/messageRoutes");
+const savedProductRoutes = require("./routes/savedProductRoutes");
+
+const initializeSocket = require("./sockets");
 
 const app = express();
 
-// =========================
-// DATABASE
-// =========================
-
 connectDB();
-
-// =========================
-// CORS
-// =========================
 
 app.use(
   cors({
     origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
 
-// =========================
-// BODY PARSER
-// =========================
-
 app.use(express.json());
-
-// =========================
-// STATIC UPLOADS
-// =========================
+app.use(cookieParser());
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-
-// =========================
-// ROUTES
-// =========================
 
 app.use("/api/products", productRoutes);
 
 app.use("/api/categories", categoryRoutes);
 
-// =========================
-// TEST API
-// =========================
+app.use("/api/auth", authRoutes);
+
+app.use("/api/conversations", conversationRoutes);
+
+app.use("/api/messages", messageRoutes);
+
+app.use("/api/saved-products", savedProductRoutes);
 
 app.get("/", (req, res) => {
   res.json({
@@ -56,12 +52,16 @@ app.get("/", (req, res) => {
   });
 });
 
-// =========================
-// SERVER
-// =========================
+const server = http.createServer(app);
 
-app.listen(3000, () => {
+const io = initializeSocket(server);
+
+app.set("io", io);
+
+server.listen(3000, () => {
   console.log("Server is running at http://localhost:3000");
+
+  console.log("Socket.IO is running");
 });
 
 module.exports = app;
